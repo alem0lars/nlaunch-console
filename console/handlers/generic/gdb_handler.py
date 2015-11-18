fd029d836772dcebed3bdef7cf355f72b0a41188
Alessandro Molari <molari.alessandro@gmail.com>
2015-11-18 17:29:16 +0100

    Fix F U C K I N G python strings hell

--- console/handlers/generic/gdb_handler.py
+++ console/handlers/generic/gdb_handler.py
@@ -1,5 +1,7 @@
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+from ast import literal_eval
 from re import match, escape, findall
+from os import seteuid, setegid
 from textwrap import dedent
 
 from handlers.generic.run_program_handler import RunProgramHandler
@@ -11,10 +13,17 @@ from misc.text import colorInfo, colorToken, colorError
 class GDBHandler(ProcessHandler):
 
     WELCOME_MSG_SUFFIX = dedent("""
-        To run the program in standalone mode (i.e. without gdb), run:
+        You can pass plain arguments or a python literal string to be evaluated.
+        To do that, encapsulate the literal between curly brackets.
+        For example:
+
+            {argCmdExample}
+
+        To run the program in standalone mode (i.e. without GDB), run:
 
             {runCommand}
     """).format(
+        argCmdExample=colorInfo('{"AAAAAAAAAAAA\xef\xbe\xad\xde"}'),
         runCommand=colorToken("!run-program <ARGS>"))
 
     """Handler for a GDB-based challenge."""
@@ -36,11 +45,24 @@ class GDBHandler(ProcessHandler):
         if match("\s*!run-program\s*", line):
             m = findall("\s*!run-program(?:\s+(.+))?", line)
             suffix = m[0]
-            self.logger.debug("Suffix: %s" % suffix)
-            args = suffix.split(" ")
+            self.logger.debug("Suffix: '%s'" % suffix)
+            args = self._buildArgs(suffix)
             args.insert(0, self.binary)
             self.manager.changeHandler(
                 RunProgramHandler(self.dal, self.manager, args, self.user,
                     self)) # Return to this handler after program termination.
             return False
         return super(GDBHandler, self)._shouldSendToSubprocess(line)
+
+    def _buildArgs(self, argsStr):
+        args = None
+
+        matches = findall(r'\{([^\}]+)\}', argsStr)
+        if len(matches) > 0:
+            # THIS FUCKING LINE WASTED 8 OF MY HOURS!!!
+            # IF YOU FUCKING DELETE IT, I WILL FUCKING BURN YOU IN THE HELL
+            args = [literal_eval(matches[0]).encode("latin1")]
+        else:
+            args = argsStr.split(" ")
+
+        return args
--- provision.sh
+++ provision.sh
@@ -15,11 +15,12 @@ _local_challenges_dir="${_local_data_dir}/challenges"
 echo ">> Provisioning started!"
 
 echo ">> Installing packages.." # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-apt-get update
-apt-get install -y "gcc" "gdb"                        # ◀─ compiler and debugger
-apt-get install -y "python"  "python-dev"  "python-virtualenv"  # ◀─ python 2
-apt-get install -y "python3" "python3-dev" "python3-virtualenv" # ◀─ python 3
-apt-get install -y "git"                                        # ◀─ vcs
+# TODO uncomment
+# apt-get update
+# apt-get install -y "gcc" "gdb"                        # ◀─ compiler and debugger
+# apt-get install -y "python"  "python-dev"  "python-virtualenv"  # ◀─ python 2
+# apt-get install -y "python3" "python3-dev" "python3-virtualenv" # ◀─ python 3
+# apt-get install -y "git"                                        # ◀─ vcs
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 echo ">> Disabling protections.."  # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -79,13 +80,14 @@ cp "${_local_challenges_dir}/pyjail/pyjail.py" "/home/level-001"
 [[ -z $(id -u "level-002" 2>/dev/null) ]] && manage-levels create_lvl 2
 cp "${_local_challenges_dir}/hellobof/hellobof.c" "/home/level-002"
 cd "/home/level-002" && \
-  gcc -fno-stack-protector -std=c1x -Wall -Wextra --pedantic "hellobof.c" -o "hellobof.elf" && \
+  gcc -z execstack -fomit-frame-pointer -fno-stack-protector -z norelro -D_FORTIFY_SOURCE=0 -std=c1x -Wall -Wextra --pedantic "hellobof.c" -o "hellobof.elf" && \
   rm "hellobof.c"
 
 [[ -z $(id -u "level-003" 2>/dev/null) ]] && manage-levels create_lvl 3
+cp "${_local_challenges_dir}/goodbad/goodbad.elf" "/home/level-003"
 cp "${_local_challenges_dir}/goodbad/goodbad.c" "/home/level-003"
 cd "/home/level-003" && \
-  gcc -fno-stack-protector -std=c1x -Wall -Wextra --pedantic "goodbad.c" -o "goodbad.elf" && \
+  gcc -z execstack -fomit-frame-pointer -fno-stack-protector -z norelro -D_FORTIFY_SOURCE=0 -std=c1x -Wall -Wextra --pedantic "goodbad.c" -o "goodbad-notworking.elf" && \
   rm "goodbad.c"
 
 [[ -z $(id -u "level-004" 2>/dev/null) ]] && manage-levels create_lvl 4
@@ -93,7 +95,6 @@ cd "/home/level-003" && \
 # Reset the ownership of level-001 password (it's a jail not a suid challenge).
 chown "level-001:level-001" "/home/level-001/002-password"
 # Suid the binaries.
-#mount -o remount,suid /
 chown "level-003:level-002" "/home/level-002/hellobof.elf"
 chmod 550 "/home/level-002/hellobof.elf"
 chmod u+s "/home/level-002/hellobof.elf"
